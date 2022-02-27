804a756058e62278472a645e7f72401ae955e873
Themri Montassar <montassar.themri1@gmail.com>
2022-02-27 21:01:25 +0100

    gallery

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  //const [count, setCount] = useState(0);
+  const [Data, setData] = useState([]);
+  //   const [a, seta] = useState(0);
+  //  console.log("ddd",a)
+  //  seta(5)
+  //  console.log("ddd",a)
+
+  useEffect(() => {
+    const getRandomPhotos = async () => {
+      const res = await axios.get(
+        "https://api.unsplash.com/photos?client_id=mtHuH_DKfH41q5T-wV3oO_DpTiUv2GahJoaIiO7OyKc"
+      );
+
+      setData(res.data);
+
+      //console.log("resultat", res.data);
+    };
+    getRandomPhotos();
+  }, []);
+  console.log("resultat", Data);
+  return (
+    <div className="pt-10">
+      <div className="text-center  text-sky-600 text-9xl font-sans antialiased font-semibold">
+        Gallery
+      </div>
+
+      <div className="input-group ">
+        <div>
+          <input
+            type="search"
+            className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="search"
+            aria-label="Search"
+            aria-describedby="button-addon2"
+          />
+        </div>
+        <div>
+          <button
+            className="btn px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+            type="button"
+            id="button-addon2"
+          >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="search"
+              className="w-4"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div className="columns-4 items-center justify-center gap-14 mx-auto h-min mb-10 space-y-14 py-10">
+        {Data.map((img) => {
+          return (
+            <Link key={img.id} to={`/${img.id}`}>
+              <div className="photos hover:-translate-y-6 duration-500 cursor-pointer shadow-md rounded-t-lg break-inside-avoid-column bg-neutral-50 mb-10">
+                <img
+                  src={img.urls.small}
+                  alt={img.user.name}
+                  className="rounded-t-lg bg-cover w-full"
+                />
+                <figcaption className="text-center">{img.user.name} +{img.likes}</figcaption>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/ImageDetails/index.jsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
+import { useParams,Link } from "react-router-dom";
+
+const ImageDetails = () => {
+  const { imageId } = useParams();
+  const [Image, setImage] = useState({});
+  const [Loading, setLoading] = useState(false);
+  const [Images, setImages] = useState([]);
+  useEffect(() => {
+    const getRandomDetails = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        `https://api.unsplash.com/photos/${imageId}?client_id=mtHuH_DKfH41q5T-wV3oO_DpTiUv2GahJoaIiO7OyKc`
+      );
+
+      setImage(res.data);
+      setLoading(false);
+    };
+    getRandomDetails();
+  }, [imageId]);
+
+  useEffect(() => {
+    const getRandomPhotos = async () => {
+      const res = await axios.get(
+        "https://api.unsplash.com/photos?client_id=mtHuH_DKfH41q5T-wV3oO_DpTiUv2GahJoaIiO7OyKc"
+      );
+
+     setImages(res.data);
+
+     // console.log("resultat", res.data);
+    };
+    getRandomPhotos();
+  }, [])
+
+
+
+
+
+  return Loading ? (
+    <div> Loading</div>
+  ) : (
+    <Fragment>
+      <div className="flex flex-col items-center justify-center  py-10 ">
+        <h1 className="py-10"> {Image?.user?.name}</h1>
+
+        <div className="bg-gray-50 box-border rounded-3xl  max-w-7xl flex flex-row flex-wrap items-center justify-center">
+          <div className="">
+            <img className="img rounded-l-lg bg-cover" src={Image?.urls?.regular} alt="" />
+          </div>
+          <div className="text-center ">
+            <h5> bio : {Image?.user?.bio}</h5> <h5> views: {Image.views}</h5>
+            <h5> downloads : {Image.downloads}</h5>{" "}
+            <h5> description : {Image.description}</h5>
+          </div>
+        </div>
+      </div>
+
+      {/* ------------ */}
+   
+      <div className="columns-4 pt-14 items-center justify-center gap-14 mx-auto h-min mb-10">
+        {Images.map((img) => {
+          return (
+            <Link key={img.id} to={`/${img.id}`}>
+              <div className="photos hover:-translate-y-6 duration-500 cursor-pointer shadow-md rounded-t-lg bg-neutral-50 mb-10 break-inside-avoid-column">
+                <img
+                  src={img.urls.small}
+                  alt={img.user.name}
+                  className="rounded-t-lg bg-cover w-full"
+                />
+                <figcaption className="text-center">
+                  {img.user.name}
+                </figcaption>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+
+
+    </Fragment>
+  );
+};
+
+export default ImageDetails;
